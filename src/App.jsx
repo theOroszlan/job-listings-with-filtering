242ab59926b04bf056245f20f8249b5c354e723d
Junior Manganye <itumelengjnr966@gmail.com>
2026-08-03 00:22:19 +0200

    feat: add job listings UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import JobCard from "./components/JobCard";
+import data from "./data/data.json";
+
 function App() {
-  return <div>Hello React!</div>;
+  const [jobs, setJobs] = useState(data);
+
+  return (
+    <div className="wrapper">
+      <div class="header-background"></div>
+      <main>
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export default App;
