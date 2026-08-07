07b7ab831abd88746706f7427e172db772ee37b0
Junior Manganye <itumelengjnr966@gmail.com>
2026-08-07 21:04:18 +0200

    fix: replace class with className for header background

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
 
   return (
     <div className="wrapper">
-      <div class="header-background"></div>
+      <div className="header-background"></div>
       <main>
         <FilterBar
           filters={filters}
